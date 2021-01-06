fc7d93e0793c3e36c163be79d2f91ee3d3d6ffcc
Rajneesh Pal <37856911+Rajnees-h@users.noreply.github.com>
2021-01-06 22:15:08 +0530

    Update heartBreak.js

--- heartBreak.js
+++ heartBreak.js
@@ -1,4 +1,9 @@
 var startButton = document.querySelector('#startGame');
+var visibleImg = "none";
+var score = 0;
+var visibleTime = 1000;
+
+
 startButton.addEventListener("click",function(){
     
     var page = document.querySelector("#main");
@@ -12,5 +17,50 @@ startButton.addEventListener("click",function(){
 
 function startGame(){
     startButton.style.display = "none";
-    displayHeart();
+    addEventListeners();
+    displayHeart(undefined);
+}
+
+
+
+function addEventListeners(){
+    var hearts = document.querySelectorAll("img");
+
+for(i=0;i<hearts.length;i++){
+    hearts[i].addEventListener("click",function(){
+        this.src="heart_break.png";
+
+
+        updateScore();
+
+        visibleImg = this;
+        displayHeart();
+    });
+}
+
+}
+
+function updateScore(){
+    var scoreElement = document.querySelector('#score');
+
+    score++;
+    scoreElement.textContent = "Score : "+score;
+   
+
+}
+function displayHeart(){
+    
+
+    if(visibleImg != "none" ){
+       
+        visibleImg.style.visibility = "hidden";
+        visibleImg.src="heart.png";
+    }
+
+    var id = "#heart"+Math.floor(Math.random(0)*12)+"";
+
+    var visibleHeart = document.querySelector(id);
+    visibleHeart.style.visibility = "visible"
+    
+
 }
